f02704f8c13294054264b6b92edba4275eb517a3
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-07-23 10:50:10 -0300

    feat: adding lombok, entity user and REST work

---
+++ src/main/java/com/jcarlos67/workshopmongo/domain/User.java
@@ -0,0 +1,43 @@
+package com.jcarlos67.workshopmongo.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class User implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Setter(AccessLevel.NONE)
+  @Id()
+  private String id;
+
+  private String name;
+  private String email;
+  
+  public User(){
+  }
+
+  public User(String id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(getId(), user.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getId());
+  }
+}
